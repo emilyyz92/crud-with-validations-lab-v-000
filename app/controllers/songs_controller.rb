436f84e1466e8c4901d7dfcf1fdfc56fa41f8ad1
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-29 15:29:39 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -23,14 +23,15 @@ class SongsController < ApplicationController
   end
 
   def edit
+
+  end
+
+  def update
     if @song.update(song_param)
       redirect_to song_path(@song)
     else
       render :edit
     end
-  end
-
-  def update
   end
 
   private
